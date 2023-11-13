223daf9721cc661303d10d84a33714959e89e4cb
jakubenglicky <jakubenglicky@gmail.com>
2023-11-13 15:16:40 +0100

    Add grafana and uptime subdomain

--- enlabs.cz/dns.tf
+++ enlabs.cz/dns.tf
@@ -5,3 +5,19 @@ resource "cloudflare_record" "monitoring" {
   type    = "A"
   proxied = true
 }
+
+resource "cloudflare_record" "uptime" {
+  zone_id = var.cloudflare_zone_id
+  name    = "uptime"
+  value = "monitoring.enlabs.cz"
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "cloudflare_record" "grafana" {
+  zone_id = var.cloudflare_zone_id
+  name    = "grafana"
+  value = "monitoring.enlabs.cz"
+  type    = "CNAME"
+  proxied = true
+}
