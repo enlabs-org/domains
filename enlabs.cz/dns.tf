1fa3fb3de59086c363732e044cc67a72f685451e
jakubenglicky <jakubenglicky@gmail.com>
2023-11-24 23:40:47 +0100

    Add subdomain posobota

--- enlabs.cz/dns.tf
+++ enlabs.cz/dns.tf
@@ -21,3 +21,11 @@ resource "cloudflare_record" "grafana" {
   type    = "CNAME"
   proxied = false
 }
+
+resource "cloudflare_record" "posobota" {
+  zone_id = var.cloudflare_zone_id
+  name    = "posobota"
+  value = "monitoring.enlabs.cz"
+  type    = "CNAME"
+  proxied = false
+}
