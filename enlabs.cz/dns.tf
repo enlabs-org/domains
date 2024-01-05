724c4b7290cb0271b4325342f6455766e8e92af5
jakubenglicky <jakubenglicky@gmail.com>
2024-01-05 22:20:52 +0100

    Add status subdomain

--- enlabs.cz/dns.tf
+++ enlabs.cz/dns.tf
@@ -29,3 +29,11 @@ resource "cloudflare_record" "posobota" {
   type    = "CNAME"
   proxied = false
 }
+
+resource "cloudflare_record" "status" {
+  zone_id = var.cloudflare_zone_id
+  name    = "status"
+  value = "monitoring.enlabs.cz"
+  type    = "CNAME"
+  proxied = false
+}
